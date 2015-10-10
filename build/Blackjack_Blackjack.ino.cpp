7338eb8d39373875686b9fe00e37a1e3f4b148d9
ben-hunter-hansen <ben.hunter.hansen@gmail.com>
2015-10-10 18:48:05 -0500

    game playable

--- build/Blackjack_Blackjack.ino.cpp
+++ build/Blackjack_Blackjack.ino.cpp
@@ -20,10 +20,16 @@
  int getButton() ;
  int computeScore(char *hand) ;
  int computeScore(char *hand) ;
+ int computeScoreNoBust(char *hand) ;
+ int computeScoreNoBust(char *hand) ;
  int hit(char *hand, char *deck, int *deckPointer) ;
  int hit(char *hand, char *deck, int *deckPointer) ;
  int stay(char *dealerHand, char *deck, int *deckPointer) ;
  int stay(char *dealerHand, char *deck, int *deckPointer) ;
+ void printGameResult(char *message, int score) ;
+ void printGameResult(char *message, int score) ;
+ void resetGame(char *playerHand, char *dealerHand, char *deck,int *playerScore,int *dealerScore, int *deckPointer, bool *checkForWinner) ;
+ void resetGame(char *playerHand, char *dealerHand, char *deck,int *playerScore,int *dealerScore, int *deckPointer, bool *checkForWinner) ;
  void setup() ;
  void setup() ;
  void loop() ;
@@ -51,7 +57,7 @@ const char CARDS[] = {
 };
 
 const int DECK_SIZE = 52;
-const int HAND_SIZE = 6; // This may need to be raised
+const int HAND_SIZE = 7; // This may need to be raised
 
 const byte RNG_SEED_PIN = A2;
 
@@ -108,19 +114,6 @@ void printCards(char *cards, unsigned int row, unsigned int col, bool faceDown) 
           lcd.print(" ?|");
         }
     }
-}
-
-/*
- *  Shifts the deck over after cards are drawn,
- *  so the first index of the deck is pointing at the next
- *  card.  This also re sizes the deck.
- */
-void shiftDeck(char deck[DECK_SIZE], int drawn) {
-    int deckSize = cardCount(deck);
-    for(int i = 0; i < deckSize; i++) {
-       deck[i] = deck[i + drawn];
-    }
-    deck[deckSize] = 0;
 }
 
 
@@ -202,7 +195,35 @@ int getButton() {
     return button;
 }
 
+/*
+ *  Computes the score for a hand of cards
+ */
 int computeScore(char *hand) {
+    int score = 0, aceCount = 0;
+    for(int i = 0; i < cardCount(hand); i++) {
+        if(hand[i] == 'T' || hand[i] == 'J' || hand[i] == 'Q' || hand[i] == 'K') {
+            score += 10;
+        } else if(hand[i] == 'A') {
+            aceCount++;
+        } else {
+            score += hand[i]-'0';
+        }
+    }
+
+    if(aceCount) {
+        for(int i = 0; i < aceCount; i++) {
+            if(aceCount > 1 || score > 10) {
+                score++;
+            } else {
+                score += 11;
+            }
+        }
+    }
+
+    return score <= 21 ? score : BUST;
+}
+
+int computeScoreNoBust(char *hand) {
     int score = 0, aceCount = 0;
     for(int i = 0; i < cardCount(hand); i++) {
         if(hand[i] == 'T' || hand[i] == 'J' || hand[i] == 'Q' || hand[i] == 'K') {
@@ -227,16 +248,24 @@ int computeScore(char *hand) {
     return score;
 }
 
-
+/*
+ *  The player chose to hit
+ */
 int hit(char *hand, char *deck, int *deckPointer) {
     drawCard(hand,deck,deckPointer);
+    int score = computeScore(hand);
+    return score > 21 ? BUST : score;
 }
 
+/*
+ *  The player chose to stay. This function represents the dealers turn,
+ *  returning the final score of the dealer to the caller.
+ */
 int stay(char *dealerHand, char *deck, int *deckPointer) {
     printCards(dealerHand,0,0,false);
 
     int dealerScore = computeScore(dealerHand);
-    if(dealerScore >= 16) { // Don't have the dealer try to hit on 16 or up
+    if(dealerScore >= 17) { // Don't have the dealer try to hit on 17 or up
         return dealerScore;
     }
 
@@ -244,11 +273,9 @@ int stay(char *dealerHand, char *deck, int *deckPointer) {
         drawCard(dealerHand,deck, deckPointer);
         dealerScore = computeScore(dealerHand);
         printCards(dealerHand,0,0,false);
-        if(dealerScore > 21) {
-            dealerScore = BUST;
+        if(dealerScore == BUST) {
             break;
-        } else if(dealerScore == 21) {
-            dealerScore = BLACKJACK;
+        } else if(dealerScore == BLACKJACK) {
             break;
         } else if(dealerScore >= 16) {
             break;
@@ -256,6 +283,32 @@ int stay(char *dealerHand, char *deck, int *deckPointer) {
     }
 
     return dealerScore;
+}
+
+void printGameResult(char *message, int score) {
+    score > 9 ? lcd.setCursor(14,1) : lcd.setCursor(15,1);
+    lcd.print(score);
+
+    lcd.setCursor(13,0);
+    lcd.print("...");
+    delay(1000);
+    lcd.setCursor(16-String(message).length(),0);
+    lcd.print(message);
+    delay(500);
+
+    Button b = (Button) getButton();
+    while(b == NO_BTN) {
+
+        b = (Button) getButton();
+    }
+}
+
+void resetGame(char *playerHand, char *dealerHand, char *deck,int *playerScore,int *dealerScore, int *deckPointer, bool *checkForWinner) {
+    shuffle(deck);
+    drawHand(dealerHand, deck, deckPointer);
+    drawHand(playerHand,deck, deckPointer);
+    *checkForWinner = false;
+    *playerScore = *dealerScore = 0;
 }
 
 void setup() {
@@ -273,13 +326,22 @@ void loop() {
     static char hand[HAND_SIZE];
     static int deckPointer = 0;
 
+    static int playerScore = 0;
+    static int dealerScore = 0;
     static bool first = true;
     static bool checkForWinner = false;
+
 
     if(first) {
         generateDeck(deck);
         drawHand(hand,deck, &deckPointer);
         drawHand(dealerHand, deck, &deckPointer);
+        playerScore = computeScore(hand);
+
+        if(playerScore == BLACKJACK) {
+            printGameResult("Blackjack!", playerScore);
+            resetGame(hand, dealerHand, deck, &playerScore, &dealerScore, &deckPointer, &checkForWinner);
+        }
         first = false;
     }
 
@@ -289,10 +351,10 @@ void loop() {
 
         switch (button) {
             case UP_BTN:
-                hit(hand,deck, &deckPointer);
+                playerScore = hit(hand,deck, &deckPointer);
                 break;
             case DOWN_BTN:
-                stay(dealerHand,deck, &deckPointer);
+                dealerScore = stay(dealerHand,deck, &deckPointer);
                 checkForWinner = true;
                 break;
             case LEFT_BTN:
@@ -300,22 +362,40 @@ void loop() {
             case RIGHT_BTN:
                 break;
             case SELECT_BTN:
-                shuffle(deck);
-                drawHand(dealerHand, deck, &deckPointer);
-                drawHand(hand,deck, &deckPointer);
-                checkForWinner = false;
+                resetGame(hand,dealerHand,deck,&playerScore,&dealerScore,&deckPointer,&checkForWinner);
                 break;
             default:
                 break;
         }
     }
 
-    if(!checkForWinner) { // Print second dealer card face down during the players turn
+    printCards(hand,1,0, false);
+
+    if(computeScore(hand) == BLACKJACK) {
+        printGameResult("Win!", playerScore);
+        resetGame(hand,dealerHand,deck,&playerScore,&dealerScore,&deckPointer,&checkForWinner);
+
+    } else if(computeScore(hand) == BUST) {
+        printGameResult("Bust!", computeScoreNoBust(hand));
+        resetGame(hand,dealerHand,deck,&playerScore,&dealerScore,&deckPointer,&checkForWinner);
+    }
+
+    if(checkForWinner) {
+        printCards(dealerHand, 0,0,false);
+        if(computeScore(hand) > computeScore(dealerHand)) {
+            printGameResult("Win!", computeScore(hand));
+        } else if(computeScore(hand) == computeScore(dealerHand)) {
+            printGameResult("Tie.", computeScore(hand));
+        } else {
+            printGameResult("Lose.", computeScore(hand));
+        }
+
+        resetGame(hand,dealerHand,deck,&playerScore,&dealerScore,&deckPointer,&checkForWinner);
+    } else {
         printCards(dealerHand,0,0,true);
     }
 
-    printCards(hand,1,0, false);
-    lcd.setCursor(14, 0);
+    computeScore(hand) > 9 ? lcd.setCursor(14,1) : lcd.setCursor(15,1);
     lcd.print(computeScore(hand));
     delay(100);
 }
